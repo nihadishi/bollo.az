021130e34eff8453c5e4fbbdb276c1a880965677
Nihad <nihadishi@gmail.com>
2023-08-08 02:20:19 +0400

    products static

--- client/src/pages/ProductsPage/ProductsPage.jsx
+++ client/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,8 +1,228 @@
 import React from 'react'
-
+import "./style.scss"
+import tomato from "./img/sampletomato.jpeg"
+import addicon from "./img/addicon.gif"
 const ProductsPage = () => {
+  usest
   return (
-    <div>ProductsPage</div>
+    <div className="Products">
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">{}</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="+" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+      <div className="Products-Product">
+        <div className="Products-Product-SaleLike">
+          <div className="Products-Product-SaleLike-Sale">20% OFF</div>
+          <div className="Products-Product-SaleLike-Like">Liked</div>
+        </div>
+        <div className="Products-Product-Photo">
+          <img src={tomato} alt="Tomato" />
+        </div>
+        <div className="Products-Product-About">
+          <div className="Products-Product-About-S">
+            <div className="Products-Product-About-S-Name">Tomato</div>
+            <div className="Products-Product-About-S-Price">20$</div>
+            <div className="Products-Product-About-S-Rating">5 5 5 5 5</div>
+          </div>
+          <div className="Products-Product-About-L"><img src={addicon} alt="" /></div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
