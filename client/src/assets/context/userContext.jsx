ca88d9cc3491d20dc00a386efb88aa67ef3b0498
Nihad <nihadishi@gmail.com>
2023-09-18 03:37:49 +0400

    bug fix delete json

--- client/src/assets/context/userContext.jsx
+++ client/src/assets/context/userContext.jsx
@@ -1,46 +1,46 @@
 import axios from "axios";
 import { createContext, useState, useEffect } from "react";
 
-export const UserContext = createContext({})
+export const UserContext = createContext({});
 
-export function UserContextProvider({children}) {
-    const initialState = localStorage.getItem("userDatas");
-   
-    const [user,setUser] = useState(JSON.parse(initialState));
-    const kullaniciVerileri = {};
-    for (const key in user) {
-        if (user.hasOwnProperty(key)) {
-            kullaniciVerileri[key] = user[key];
-        }
+export function UserContextProvider({ children }) {
+  const initialState = localStorage.getItem("userDatas");
+
+  const [user, setUser] = useState(initialState);
+  const kullaniciVerileri = {};
+
+  for (const key in user) {
+    if (user.hasOwnProperty(key)) {
+      kullaniciVerileri[key] = user[key];
     }
-    console.log("user",user)
-    const [loading,setLoading] = useState(true);
-    
-    useEffect(() => {
-        
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('user/profile');
-                setUser(response.data);
-                
-            } catch (error) {
-                console.error(error);
-            } 
-            // finally {
-            //     setLoading(false);
-            // }
-        };
+  }
 
-        if (!user) {
-            fetchData();
-        }
-        localStorage.setItem("userDatas",kullaniciVerileri);
-    }, [user])
-    // console.log("user",user);
-    return(
-        <UserContext.Provider value={{user,setUser,loading,setLoading}}>
-            {children}
-        </UserContext.Provider>
-    )
-    
+  console.log("user", user);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("user/profile");
+        setUser(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+      // finally {
+      //     setLoading(false);
+      // }
+    };
+
+    if (!user) {
+      fetchData();
+    }
+    console.log("userData",kullaniciVerileri);
+    localStorage.setItem("userDatas", kullaniciVerileri);
+  }, [user]);
+  // console.log("user",user);
+  return (
+    <UserContext.Provider value={{ user, setUser, loading, setLoading }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
