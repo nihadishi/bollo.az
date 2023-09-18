cbc16a7782e973856ae51ab8e0279a9ae974d9f5
Nihad <nihadishi@gmail.com>
2023-09-18 23:54:46 +0400

    bug fix context useeffect

--- client/src/assets/context/userContext.jsx
+++ client/src/assets/context/userContext.jsx
@@ -4,8 +4,6 @@ import { createContext, useState, useEffect } from "react";
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
-//   const initialState = localStorage.getItem("userDatas");
-// const [user, setUser] = useState(JSON.parse(initialState));
 
 const initialState = null;
 const [user, setUser] = useState(initialState);
@@ -14,34 +12,22 @@ const [user, setUser] = useState(initialState);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("user/profile");
-        setUser(response.data);
-      } catch (error) {
-        console.error(error);
+        axios.get('user/profile')
+            .then(({data})=>{
+                setUser(data)
+            }).catch(error =>{}
+             )
+  }, []);
+  useEffect(() => {
+    if(!user){
+        axios.get('user/profile')
+            .then(({data})=>{
+                // console.log("contex",data)
+                setUser(data)
+            }).catch(error =>{}
+             )
       }
-    };
-
-    if (!user) {
-      fetchData();
-    }
     console.log("userr",user);
-    // localStorage.setItem(
-    //   "userDatas",
-    //   JSON.stringify({
-    //     city: user?.city,
-    //     createdAt: user?.createdAt,
-    //     email: user?.email,
-    //     fullname: user?.fullname,
-    //     image: user?.image,
-    //     number: user?.number,
-    //     password: user?.password,
-    //     region: user?.region,
-    //     __v: 0,
-    //     _id: user?._id,
-    //   })
-    // );
   }, [user]);
   return (
     <UserContext.Provider value={{ user, setUser, loading, setLoading }}>
